524aae55abdaf93c71ce2fa7fb13016e91e3f504
Paulo Luan <pauloluan.inova@gmail.com>
2021-08-31 06:08:45 -0300

    ♻️ refactor: put all sources into src

--- rn-storybook/App.tsx
+++ rn-storybook/App.tsx
@@ -1,28 +1,4 @@
+import { App } from './src'
 import StorybookUI from './storybook'
-import Config from 'react-native-config'
-
-import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
-
-import useCachedResources from './hooks/useCachedResources'
-import useColorScheme from './hooks/useColorScheme'
-import Navigation from './navigation'
-
-function App() {
-  const isLoadingComplete = useCachedResources()
-  const colorScheme = useColorScheme()
-
-  if (!isLoadingComplete) {
-    return null
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    )
-  }
-}
 
 export default __DEV__ ? StorybookUI : App
